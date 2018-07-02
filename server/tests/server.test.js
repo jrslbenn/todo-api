26a5e312c381b97acdee9af3b6df217f98a14ebd
Bennett <jbennett1@ebay.com>
2018-07-02 14:53:24 -0700

    add tests for delete /todos/id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,9 +1,15 @@
 const expect = require("expect");
 const request = require("supertest");
 
-const {ObjectID} = require("mongodb");
-const {app} = require("./../server");
-const {Todo} = require("./../models/todo");
+const {
+  ObjectID
+} = require("mongodb");
+const {
+  app
+} = require("./../server");
+const {
+  Todo
+} = require("./../models/todo");
 
 const todos = [{
   _id: new ObjectID(),
@@ -25,7 +31,9 @@ describe("POST /todos", () => {
 
     request(app)
       .post("/todos")
-      .send({text})
+      .send({
+        text
+      })
       .expect(200)
       .expect((res) => {
         expect(res.body.text).toBe(text);
@@ -35,7 +43,9 @@ describe("POST /todos", () => {
           return done(err);
         }
 
-        Todo.find({text}).then((todos) => {
+        Todo.find({
+          text
+        }).then((todos) => {
           expect(todos.length).toBe(1);
           expect(todos[0].text).toBe(text);
           done();
@@ -104,4 +114,45 @@ describe("GET /todos/:id", () => {
       .expect(404)
       .end(done);
   });
+});
+
+describe("GET /todos/:id", () => {
+  it("should remove a todo", (done) => {
+    var hexId = todos[1]._id.toHexString();
+
+    request(app)
+      .delete(`/todos/${hexId}`)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo._id).toBe(hexId);
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        Todo.findById(hexId).then((todo) => {
+          expect(todo).toNotExist();
+          done();
+        }).catch((e) => done(e));
+      });
+    });
+
+    it("should return 404 if todo not found", (done) => {
+      bogus_id = new ObjectID()
+      console.log(bogus_id);
+      request(app)
+        .delete(`/todos/${bogus_id.toHexString()}`)
+        .expect(404)
+        .end(done);
+    });
+
+    it("should return a 404 for non object ids", (done) => {
+      bogus_id = new ObjectID()
+      console.log(bogus_id);
+      request(app)
+        .delete("/todos/123abc")
+        .expect(404)
+        .end(done);
+    });
 });
