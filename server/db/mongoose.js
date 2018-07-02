43da19bd99cc01be22442abe11566f3b983f747c
Bennett <jbennett1@ebay.com>
2018-07-02 14:19:42 -0700

    setup remote monngo and heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://localhost:27017/TodoApp");
+mongoose.connect( "mongodb://user:password1@ds018538.mlab.com:18538/todo" || "mongodb://localhost:27017/TodoApp");
 
 module.exports = {mongoose};
