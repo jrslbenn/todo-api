0e9a0005f58b5884234f67b71d2cec0e04eaaa05
Bennett <jbennett1@ebay.com>
2018-07-02 16:03:30 -0700

    setup separate test and development envs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 mongoose.Promise = global.Promise;
 // Local db
-mongoose.connect("mongodb://localhost:27017/TodoApp");
+mongoose.connect(process.env.MONGODB_URI);
 // remote db
 //mongoose.connect("mongodb://user:password1@ds018538.mlab.com:18538/todo");
 
